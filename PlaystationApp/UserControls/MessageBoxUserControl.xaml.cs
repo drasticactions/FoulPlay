8686c7c198cc1c1ff52f619d32730686758281e5
drasticactions <innerlogic4321@gmail.com>
2013-12-13 08:47:10 -0500

    Changes to Message User Control view
    Remove Microphone references

--- PlaystationApp/UserControls/MessageBoxUserControl.xaml.cs
+++ PlaystationApp/UserControls/MessageBoxUserControl.xaml.cs
@@ -4,7 +4,6 @@ using PlaystationApp.Core.Manager;
 using System.IO;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace PlaystationApp.UserControls
